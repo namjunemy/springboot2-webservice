8dafb317b94f396652f879de509d33e50c88ae03
Namjun Kim <namjunemy@gmail.com>
2019-12-23 00:26:00 +0900

    Lombok과 Dto 테스트

--- src/test/java/io/namjune/springboot/web/HelloControllerTest.java
+++ src/test/java/io/namjune/springboot/web/HelloControllerTest.java
@@ -8,9 +8,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -30,5 +32,20 @@ class HelloControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    @DisplayName("helloDto 반환")
+    void returnHelloDto() throws Exception {
+        String name = "hello";
+        int amount = 10000;
+
+        mockMvc.perform(get("/hello/dto")
+                                .param("name", name)
+                                .param("amount", String.valueOf(amount)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
