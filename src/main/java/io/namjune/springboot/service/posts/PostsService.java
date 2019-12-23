0c6ec4a23717e789d0060a66cf7b2e4cb8033411
Namjun Kim <namjunemy@gmail.com>
2019-12-23 21:39:42 +0900

    Posts 등록 API 추가, 테스트 코드 작성

---
+++ src/main/java/io/namjune/springboot/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package io.namjune.springboot.service.posts;
+
+import io.namjune.springboot.domain.posts.PostsRepository;
+import io.namjune.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/io/namjune/springboot/web/PostsApiController.java
@@ -0,0 +1,20 @@
+package io.namjune.springboot.web;
+
+import io.namjune.springboot.service.posts.PostsService;
+import io.namjune.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return this.postsService.save(requestDto);
+    }
+}
