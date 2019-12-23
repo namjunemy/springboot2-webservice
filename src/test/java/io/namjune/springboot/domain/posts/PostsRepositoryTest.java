0ee3b4ef532c81d4b5c9de63d691b18dfeab0646
Namjun Kim <namjunemy@gmail.com>
2019-12-24 00:19:23 +0900

    JpaAuditing 자동화 설정

--- src/test/java/io/namjune/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/io/namjune/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,5 +1,6 @@
 package io.namjune.springboot.domain.posts;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,12 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class PostsRepositoryTest {
 
     @Autowired
@@ -42,5 +45,25 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2019, 12, 22, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                                     .title("title")
+                                     .content("content")
+                                     .author("author")
+                                     .build());
+
+        //when
+        List<Posts> postsLists = postsRepository.findAll();
+
+        //then
+        Posts posts = postsLists.get(0);
+        log.info(">>>>>>> createDate={}, modifiedDate={}", posts.getCreatedDate(), posts.getModifiedDate());
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
