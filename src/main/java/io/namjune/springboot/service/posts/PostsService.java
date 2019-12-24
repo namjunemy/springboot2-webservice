60f8cd3da01254d0e0e1a4de50a33b0abac1194a
Namjun Kim <namjunemy@gmail.com>
2019-12-25 02:33:15 +0900

    mustache 공용 레이이웃 분리, 글 등록 & 전체 목록 조회 화면 추가

--- src/main/java/io/namjune/springboot/service/posts/PostsService.java
+++ src/main/java/io/namjune/springboot/service/posts/PostsService.java
@@ -2,18 +2,28 @@ package io.namjune.springboot.service.posts;
 
 import io.namjune.springboot.domain.posts.Posts;
 import io.namjune.springboot.domain.posts.PostsRepository;
+import io.namjune.springboot.web.dto.PostsListResponseDto;
 import io.namjune.springboot.web.dto.PostsResponseDto;
 import io.namjune.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class PostsService {
 
     private final PostsRepository postsRepository;
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
 
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
---
+++ src/main/java/io/namjune/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,22 @@
+package io.namjune.springboot.web.dto;
+
+import io.namjune.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts posts) {
+        this.id = posts.getId();
+        this.title = posts.getTitle();
+        this.author = posts.getAuthor();
+        this.modifiedDate = posts.getModifiedDate();
+    }
+}
