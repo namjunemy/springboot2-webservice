45d780302cf94e3da74bd1620f17acb2489b05c3
Namjun Kim <namjunemy@gmail.com>
2019-12-26 18:24:30 +0900

    로그인 검증, 화면 추가

--- src/main/java/io/namjune/springboot/web/IndexController.java
+++ src/main/java/io/namjune/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package io.namjune.springboot.web;
 
+import io.namjune.springboot.config.auth.dto.SessionUser;
 import io.namjune.springboot.service.posts.PostsService;
 import io.namjune.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,11 +9,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/posts/update/{id}")
     public String postsUpdate(@PathVariable Long id, Model model) {
@@ -29,6 +33,10 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 }
