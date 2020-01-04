a82bf3475ffabf63e4ecfc8839f192e8ab3ff22f
Namjun Kim <namjunemy@gmail.com>
2020-01-04 21:43:00 +0900

    무중단 배포 스크립트 수정, 무중단 테스트

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# bash는 return value가 안되니 *제일 마지막줄에 echo로 해서 결과 출력*후, 클라이언트에서 값을 사용한다
 # 쉬고 있는 profile 찾기: real1이 사용중이면 real2는 미사용중, 반대면 real1이 미사용중
 
 function find_idle_profile()
